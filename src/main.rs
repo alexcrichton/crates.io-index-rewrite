1694fcec45d13fff505f49c81fd7f69777c6360c
Alex Crichton <alex@alexcrichton.com>
2018-03-06 16:37:11 -0800

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,36 @@
+use std::fs;
+use std::io::{self, Write};
+use std::process::Command;
+use std::time::{Duration, Instant};
+
+fn main() {
+    print!("A completely fresh checkout of the index takes...");
+    io::stdout().flush().unwrap();
+    let start = Instant::now();
+    let status = Command::new("cargo")
+        .current_dir("snapshot")
+        .arg("update")
+        .env("CARGO_HOME", "tmp")
+        .output()
+        .unwrap();
+    assert!(status.status.success());
+    print(start.elapsed());
+    fs::remove_dir_all("snapshot/tmp").unwrap();
+
+    print!("Cloning an index with a history of length 1 takes...");
+    io::stdout().flush().unwrap();
+    let start = Instant::now();
+    let status = Command::new("cargo")
+        .current_dir("squashed")
+        .arg("update")
+        .env("CARGO_HOME", "tmp")
+        .output()
+        .unwrap();
+    assert!(status.status.success());
+    print(start.elapsed());
+    fs::remove_dir_all("squashed/tmp").unwrap();
+}
+
+fn print(dur: Duration) {
+    println!(" {}.{:03}s", dur.as_secs(), dur.subsec_nanos() / 1_000_000);
+}
