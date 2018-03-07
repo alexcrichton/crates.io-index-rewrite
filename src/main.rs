bf0ae9fde40a10764a37f18a8d970b6a5cdeee56
Alex Crichton <alex@alexcrichton.com>
2018-03-06 16:41:36 -0800

    remove-dir-all crate

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use std::fs;
+extern crate remove_dir_all;
+
 use std::io::{self, Write};
 use std::process::Command;
 use std::time::{Duration, Instant};
@@ -15,7 +16,7 @@ fn main() {
         .unwrap();
     assert!(status.status.success());
     print(start.elapsed());
-    fs::remove_dir_all("snapshot/tmp").unwrap();
+    remove_dir_all::remove_dir_all("snapshot/tmp").unwrap();
 
     print!("Cloning an index with a history of length 1 takes...");
     io::stdout().flush().unwrap();
@@ -28,7 +29,7 @@ fn main() {
         .unwrap();
     assert!(status.status.success());
     print(start.elapsed());
-    fs::remove_dir_all("squashed/tmp").unwrap();
+    remove_dir_all::remove_dir_all("squashed/tmp").unwrap();
 }
 
 fn print(dur: Duration) {
